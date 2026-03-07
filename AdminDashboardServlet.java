aebf607c3ddb4ca09a2b564e37e728711835152e
LeDangDuyw <ledangduy0904@gmail.com>
2026-03-08 00:59:02 +0700

    Update AdminDashboardServlet.java

--- AdminDashboardServlet.java
+++ AdminDashboardServlet.java
@@ -7,7 +7,9 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import jakarta.servlet.annotation.WebServlet;
 
+@WebServlet(name = "AdminDashboardServlet", urlPatterns = {"/admin/dashboard"})
 public class AdminDashboardServlet extends HttpServlet {
 
     @Override
